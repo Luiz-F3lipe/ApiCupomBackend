f7529ffabbdef0eb0751c2702e93f8f3bdd370f9
asda <luiz.felipe.moraes.lima.1@gmail.com>
2022-08-20 16:59:30 -0300

    Deploy Heroku

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import { AppDataSource } from './data-souce'
 import routes from './routes'
 
 //Cria o express após iniciar o banco de dados
-//AppDataSource.initialize()
+AppDataSource.initialize().then(() => {
+    const app = express()
 
-const app = express()
+    const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000
+    app.use(express.json())
 
-app.use(express.json())
+    app.get('/', (req, res) => {
+        return res.send("Brenda eu te amo <3")
+    })
 
-app.get('/', (req, res) => {
-    return res.send("Brenda eu te amo <3")
+    app.use(routes)
+
+    return app.listen(port)
 })
-
-app.use(routes)
-
-app.listen(port)
