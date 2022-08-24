13d47e7809ecc36b37389abbb7db625dbfedefa9
Luiz-F3lipe <luiz.felipe.moraes.lima.1@gmail.com>
2022-08-24 16:31:52 -0300

    Ajuste Error

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import express from 'express'
-import { AppDataSource } from './data-souce'
-import routes from './routes'
+import 'express-async-errors';
+import express from 'express';
+import { AppDataSource } from './data-souce';
+import { errorMiddleware } from './middlewares/error';
+import routes from './routes';
 
 //Cria o express após iniciar o banco de dados
 AppDataSource.initialize().then(() => {
-    const app = express()
+    const app = express();
 
-    const port = process.env.PORT || 3000
+    const port = process.env.PORT || 3000;
 
-    app.use(express.json())
+    app.use(express.json());
 
     app.get('/', (req, res) => {
-        return res.json({ msg: "Api works!" })
+        return res.json({ msg: "Api works!" });
     })
 
-    app.use(routes)
+    app.use(routes);
 
-    return app.listen(port)
+    app.use(errorMiddleware);
+    return app.listen(port);
 })
